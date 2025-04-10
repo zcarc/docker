6f17aee7f3b6ae6c1f0014c3e94835b47c6310bd
Hyunsoo Lee <zcarc@naver.com>
2025-04-11 03:54:40 +0900

    .

--- springboot-mysql-jpa-demo/src/main/java/com/example/demo/AppController.java
+++ springboot-mysql-jpa-demo/src/main/java/com/example/demo/AppController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("/")
     public String index() {
+        redisTemplate.opsForValue().set("abc", "def");
         return "Hello World!";
     }
 }
